3e646ef899d63ec535b027d60d39c910a2d0feb0
Danilo Shizato <daniloshinzato@gmail.com>
2023-01-20 20:48:57 -0300

    Barra de vida criada e comentários

--- game.js
+++ game.js
@@ -1,14 +1,15 @@
 class Game {
   constructor() {
-    this.selection = 10;
+    this.selection = 10; // quantos personagens selecionaveis
   }
 
   genSelection(board) {
+    // criação do tabuleiro
     for (let i = 0; i < this.selection; i++) {
-      const card = document.createElement("div");
-      card.classList.add("card", "personagem");
+      const card = document.createElement("div"); // criação de divs relacionado a quantidade de personagens
+      card.classList.add("card", "personagem"); // adicionar classe as divs (card e personagem)
 
-      board.appendChild(card);
+      board.appendChild(card); // section do tabuleiro adota card criado na linha acima
     }
   }
 }
--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 const board = document.getElementById("board");
-const header = document.getElementById("header");
-const start = document.getElementById("start");
-const personagem = document.getElementsByClassName("personagem");
+const header = document.getElementById("header"); // cabeçalho
+const start = document.getElementById("start"); // botão Start
+const personagem = document.getElementsByClassName("personagem"); //
+const health = document.getElementById("health");
 
 start.addEventListener("click", () => {
+  // Esconder cabeçalho e aparecer seleção de personagens
   header.classList.add("hidden");
   const game = new Game();
   game.genSelection(board);
 });
 
 personagem.addEventListener("click", () => {
+  // escolher personagem e adicionar as casses de botão para estilizar
   const confirmCharacter = document.createElement("button");
   confirmCharacter.classList.add("confirmButton", "btn-3d");
-  board.appendChild(confirmCharacter);
+  board.appendChild(confirmCharacter); // no fim adicionar o botão ao final do tabuleiro
 });
+
+// health.value -= 10;
